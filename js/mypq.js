3e28dfeef2d1f75c6ad3230d111290d245f4e990
Adrian Munteanu <munteanu92@yahoo.com>
2015-02-03 16:57:46 +0200

    update

--- js/mypq.js
+++ js/mypq.js
@@ -6,7 +6,7 @@ DoublyLinkedList.prototype = {
 	constructor: DoublyLinkedList,
 	insertAfter: function(prev, info){
 		++this.length;
-		var node = {info: info, time: new Date().getTime()};
+		var node = {info: info, time: performance.now()};
 		if(prev == null){
 			node.prev = null;
 			node.next = this.first;
@@ -40,46 +40,101 @@ DoublyLinkedList.prototype = {
 		}else{
 			node.next.prev = node.prev;
 		}
+	},
+	log: function(){
+		var text = "";
+		for(var q=this.first; q!=null; q=q.next){
+			text += q.info.key+" ";
+		}
+		console.log(text);
 	}
 };
 var PriorityQueue = function (priorityCompare){
 	this._rootList = new DoublyLinkedList();
 	this._compare = priorityCompare;
 	this._min = null;
+	this._valMap = new Map();
 	this.length = 0;
 };
 PriorityQueue.prototype = {
 	constructor: PriorityQueue,
 	insertUpdate: function(value, priority){
-		//if exists => change key
-		var node = {val: value, key: priority, rank: 0, children: [], mark: false, parent: null};
+		//if exists change key
+		if(typeof this._valMap.get(value) !== "undefined"){
+			var node = this._valMap.get(value);
+			if(this._compare(priority, node.key)){
+				//decrease-key
+				if(node.parent != null && this._compare(priority, node.parent.key)){
+					//heap order violated
+					var p = node.parent;
+					//cut tree rooted at node
+					p.children.remove(node.parentListNode);
+					node.parentListNode = null;
+					node.parent = null;
+					//update rank?? TODO
+					//meld into root list
+					this._rootList.insertAfter(null, node);
+					var ln = this._rootList.first;
+					node.rootNode = ln;
+					//unmark
+					node.mark = false;
+					var q = node;
+					while(p.mark){
+						q = p;
+						p = p.parent;
+						//cut p
+						p.children.remove(q.parentListNode);
+						q.parentListNode = null;
+						q.parent = null;
+						//meld into root list
+						this._rootList.insertAfter(null, q);
+						q.rootNode = this._rootList.first;
+						//unmark
+						q.mark = false;
+					}
+					if(p.parent != null)
+						p.mark = true;
+				}
+				this._rootList.log();
+			}else{
+				//increase-key
+			}
+			node.key = priority;
+			if(this._compare(priority, this._min.key)){
+				this._min = node;
+			}
+			return;
+		}
+		var node = {val: value, key: priority, rank: 0, children: new DoublyLinkedList(), mark: false, parent: null};
+		this._valMap.set(value, node);
 		this._rootList.insertAfter(null, node);
 		var ln = this._rootList.first;
-		if(this._min == null || this._compare(node.key, this._min.info.key)){
-			this._min = ln;
+		node.rootNode = ln;
+		if(this._min == null || this._compare(node.key, this._min.key)){
+			this._min = node;
 		}
 		++this.length;
 	},
 	peek: function(){
-		return {value: this._min.info.val, priority: this._min.info.key};
+		if(this.length == 0)
+			return;
+		return {value: this._min.val, priority: this._min.key};
 	},
 	pop: function(){
 		var ret = this.peek();
+		if(typeof ret === "undefined")
+			return;
 		//delete min
-		this._rootList.remove(this._min);
+		this._rootList.remove(this._min.rootNode);
 		//meld its children into root list
-		for(var i=0; i<this._min.info.children.length; ++i){
-			this._rootList.insertAfter(null, this._min.info.children[i]);
+		for(var q=this._min.children.first; q!=null; q=q.next){
+			this._rootList.insertAfter(null, q.info);
+			var ln = this._rootList.first;
+			q.info.parent = null;
+			q.info.parentListNode = null;
+			q.info.rootNode = ln;
 		}
-		this._min.info.children = null;
-		//update min
-		var min = null;
-		for(var q=this._rootList.first; q!=null; q=q.next){
-			if(min == null || this._compare(q.info.key, min.info.key)){
-				min = q;
-			}
-		}
-		this._min = min;
+		this._min.children = new DoublyLinkedList();
 		//consolidate trees, no two roots have same rank
 		var rankArray = [];
 		for(var q=this._rootList.first; q!=null;){
@@ -93,27 +148,54 @@ PriorityQueue.prototype = {
 			var qi;
 			if(this._compare(q.info.key, lq.info.key)){
 				qi = q.info;
-				qi.children.push(lq.info);
+				qi.children.insertAfter(null, lq.info);
+				lq.info.parentListNode = qi.children.first;
 				lq.info.parent = qi;
-				qi.rank = ?? //TODO: ....
 			}else{
 				qi = lq.info;
-				qi.children.push(q.info);
+				qi.children.insertAfter(null, q.info);
+				q.info.parentListNode = qi.children.first;
 				q.info.parent = qi;
 			}
 			this._rootList.remove(lq);
+			lq.info.rootNode = null;
+			++qi.rank;
+			qi.parent = null;
+			qi.rootNode = q;
 			q.info = qi;
 			rankArray[qr] = null;
+		}
+		//update min
+		var min = null;
+		for(var q=this._rootList.first; q!=null; q=q.next){
+			if(min == null || this._compare(q.info.key, min.info.key)){
+				min = q;
+			}
+		}
+		if(min == null){
+			this._min = null;
+		}else{
+			this._min = min.info;
 		}
 		--this.length;
 		return ret;
 	}
 };
 var testpq = new PriorityQueue(function(a, b){return a<b;});
-testpq.insertUpdate("a", 5);
-testpq.insertUpdate("b", 6);
-testpq.insertUpdate("c", 2);
-testpq.insertUpdate("d", 3);
-testpq.insertUpdate("e", 7);
-testpq.insertUpdate("f", 1);
-testpq.insertUpdate("g", 4);
+testpq.insertUpdate("a",    1);
+testpq.insertUpdate("b",   14);
+testpq.insertUpdate("c", 12.5);
+testpq.insertUpdate("d", 13.4);
+testpq.insertUpdate("e",  9.3);
+testpq.insertUpdate("f",   19);
+testpq.insertUpdate("g", 11.2);
+testpq.insertUpdate("h",    8);
+testpq.insertUpdate("i",  7.1);
+testpq.insertUpdate("j",   13);
+testpq.insertUpdate("k",    7);
+testpq.insertUpdate("l",   20);
+testpq.insertUpdate("m",    6);
+testpq.insertUpdate("n",   11);
+testpq.insertUpdate("o",    9);
+testpq.insertUpdate("p",   12);
+testpq.insertUpdate("q",    5);
